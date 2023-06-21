922b5b6ddc26284f969cc5ae46c64a116c681905
LOUDINI SOUAD <souad.llddnn@gmail.com>
2023-06-21 13:18:07 +0100

    comments have been fixed

--- test/MyToken.t.sol
+++ test/MyToken.t.sol
@@ -2,22 +2,22 @@
 
 pragma solidity ^0.8.13;
 
-
 import "forge-std/Test.sol";
 import "../src/MyToken.sol";
 
 contract MyTokenTest is Test {
     MyToken public myToken;
-function setUp() public {
-    myToken = new MyToken();
-}
 
-function testInitialSupply() public {
-    assertEq(myToken.totalSupply(), 0, "Initial supply should be zero");
-}
+    function setUp() public {
+        myToken = new MyToken();
+    }
 
-function testTokenNameAndSymbol() public {
-    assertEq(myToken.name(), "MyToken", "Token name is incorrect");
-    assertEq(myToken.symbol(), "LDN", "Token symbol is incorrect");
-}
+    function testInitialSupply() public {
+        assertEq(myToken.totalSupply(), 0, "Initial supply should be zero");
+    }
+
+    function testTokenNameAndSymbol() public {
+        assertEq(myToken.name(), "MyToken", "Token name is incorrect");
+        assertEq(myToken.symbol(), "LDN", "Token symbol is incorrect");
+    }
 }

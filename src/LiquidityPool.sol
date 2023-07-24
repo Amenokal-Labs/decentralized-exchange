6235bfa609df6aa47f2f71e4870ac1c7a44fc886
LOUDINI SOUAD <souad.llddnn@gmail.com>
2023-07-25 00:45:42 +0100

    fix the exchange rate

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-
-import "../src/MyToken.sol";
+import "./MyToken.sol";
 
 contract LiquidityPool {
     MyToken public tokenA;
@@ -11,19 +10,28 @@ contract LiquidityPool {
     mapping(address => uint256) public balancesA;
     mapping(address => uint256) public balancesB;
 
-    uint256 public exchangeRate; // Storage variable for the exchange rate
+    uint256 public exchangeRate;
+
+    event Deposit(address indexed user, uint256 amountA, uint256 amountB, uint256 liquidity);
+    event Exchange(address indexed user, uint256 amountIn, uint256 amountOut, bool fromAToB);
 
     constructor(MyToken _tokenA, MyToken _tokenB, uint256 initialRate) {
         tokenA = _tokenA;
         tokenB = _tokenB;
-        exchangeRate = initialRate; 
+        exchangeRate = initialRate;
     }
 
-    // Update the exchange rate based on the constant product formula
     function updateExchangeRate() internal {
-        require(balancesA[msg.sender] > 0 && balancesB[msg.sender] > 0, "Insufficient liquidity");
+        uint256 totalBalanceA = tokenA.balanceOf(address(this));
+        uint256 totalBalanceB = tokenB.balanceOf(address(this));
 
-        exchangeRate = balancesB[msg.sender] / balancesA[msg.sender];
+        require(totalBalanceA > 0 && totalBalanceB > 0, "Insufficient liquidity");
+        // the constant product formula which equl to : K = x * y
+        uint256 totalReserveWithFees = totalBalanceA * totalBalanceB;
+        uint256 feeAmount = totalReserveWithFees * 3 / 1000;
+        uint256 totalReserveWithoutFees = totalReserveWithFees - feeAmount;
+
+        exchangeRate = totalReserveWithoutFees / totalBalanceA;
     }
 
     function deposit(uint256 amountA, uint256 amountB) external {
@@ -35,47 +43,45 @@ contract LiquidityPool {
         tokenA.transferFrom(msg.sender, address(this), amountA);
         tokenB.transferFrom(msg.sender, address(this), amountB);
 
-        updateExchangeRate(); 
-    }
+        if (exchangeRate == 0) {
+            updateExchangeRate();
+        }
 
-    function withdraw(uint256 amountA, uint256 amountB) external {
-        require(amountA > 0 && amountB > 0, "Amount must be greater than zero");
-        require(amountA <= balancesA[msg.sender] && amountB <= balancesB[msg.sender], "Insufficient balance");
-
-        balancesA[msg.sender] -= amountA;
-        balancesB[msg.sender] -= amountB;
-
-        tokenA.transfer(msg.sender, amountA);
-        tokenB.transfer(msg.sender, amountB);
-
-        updateExchangeRate(); 
+        emit Deposit(msg.sender, amountA, amountB, amountA + amountB);
     }
 
     function exchangeTokens(uint256 amount, bool fromAToB) external {
         require(amount > 0, "Amount must be greater than zero");
 
+        uint256 amountOut;
+        uint256 amountIn;
+
         if (fromAToB) {
             require(balancesA[msg.sender] >= amount, "Insufficient tokenA balance");
 
-            uint256 amountB = amount * exchangeRate;
+            amountOut = (amount * exchangeRate * 970) / 1000;
+            amountIn = amount;
 
             balancesA[msg.sender] -= amount;
-            balancesB[msg.sender] += amountB;
+            balancesB[msg.sender] += amountOut;
 
             tokenA.transferFrom(msg.sender, address(this), amount);
-            tokenB.transfer(msg.sender, amountB);
+            tokenB.transfer(msg.sender, amountOut);
         } else {
             require(balancesB[msg.sender] >= amount, "Insufficient tokenB balance");
 
-            uint256 amountA = amount / exchangeRate;
+            amountOut = (amount * 1000) / (exchangeRate * 970);
+            amountIn = amount;
 
             balancesB[msg.sender] -= amount;
-            balancesA[msg.sender] += amountA;
+            balancesA[msg.sender] += amountOut;
 
             tokenB.transferFrom(msg.sender, address(this), amount);
-            tokenA.transfer(msg.sender, amountA);
+            tokenA.transfer(msg.sender, amountOut);
         }
 
         updateExchangeRate();
+
+        emit Exchange(msg.sender, amountIn, amountOut, fromAToB);
     }
 }
--- test/LiquidityPool.t.sol
+++ test/LiquidityPool.t.sol
@@ -5,41 +5,15 @@ import "forge-std/Test.sol";
 import "../src/MyToken.sol";
 import "../src/LiquidityPool.sol";
 
+
 contract LiquidityPoolTest is Test {
     MyToken public tokenA;
     MyToken public tokenB;
     LiquidityPool public pool;
 
-    function setUp() public {
-    tokenA = new MyToken("Token A", "MTKA");
-    tokenB = new MyToken("Token B", "MTKB");
-    uint256 initialRate = 2;
-    pool = new LiquidityPool(tokenA, tokenB, initialRate); 
+    function beforeEach() public {
+        tokenA = new MyToken("TokenA", "MTK"); 
+        tokenB = new MyToken("TokenB", "MTK"); 
+        pool = new LiquidityPool(tokenA, tokenB, 2); 
     }
-
-    function testDeposit() public {
-        uint256 amountA = 1000;
-        uint256 amountB = 2000;
-
-       
-        tokenA.mint(address(this), amountA);
-        tokenB.mint(address(this), amountB);
-
-       
-        tokenA.approve(address(pool), amountA);
-        tokenB.approve(address(pool), amountB);
-
-        
-        assertEq(tokenA.balanceOf(address(this)), amountA);
-        assertEq(tokenB.balanceOf(address(this)), amountB);
-        assertEq(tokenA.balanceOf(address(pool)), 0);
-        assertEq(tokenB.balanceOf(address(pool)), 0);
-
-        pool.deposit(amountA, amountB);
-        assertEq(tokenA.balanceOf(address(this)), 0);
-        assertEq(tokenB.balanceOf(address(this)), 0);
-        assertEq(tokenA.balanceOf(address(pool)), amountA);
-        assertEq(tokenB.balanceOf(address(pool)), amountB);
-    }
-    
 }
